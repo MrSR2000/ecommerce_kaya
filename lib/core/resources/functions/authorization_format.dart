98ab894b4e987fb5319a330c79088c2e4a72adc8
SauravR2000 <saurav@apptechnologies.co>
2024-03-04 16:11:45 +0545

    bearer token formatter

---
+++ lib/core/resources/functions/authorization_format.dart
@@ -0,0 +1,18 @@
+import 'dart:developer';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:kaya/core/resources/functions/print_long_string.dart';
+
+import '../../../injection_container.dart';
+import '../../constants/constants.dart';
+
+Future<String> getAccessTokenFormat() async {
+  String? accessToken =
+      await sl<FlutterSecureStorage>().read(key: accessTokenKey);
+
+  // log("accessToken = $accessToken!");
+
+  printLongString(accessToken!);
+
+  return "Bearer $accessToken";
+}
