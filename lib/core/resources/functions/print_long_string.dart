8dc2090444b931f2122c9dd36a69550ce094facc
SauravR2000 <saurav@apptechnologies.co>
2024-03-04 16:11:52 +0545

    print long string

---
+++ lib/core/resources/functions/print_long_string.dart
@@ -0,0 +1,5 @@
+/// Print Long String
+void printLongString(String text) {
+  final RegExp pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
+  pattern.allMatches(text).forEach((RegExpMatch match) =>   print(match.group(0)));
+}
